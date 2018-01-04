0b913523abedafbd3a513424b10d3c6f8bcea917
uzername <ivanpost777@gmail.com>
2018-01-04 17:24:13 +0200

    This code is taken from bench_client.c; It does stress-testing (floods server with messages): a TCP client is being initialized on stack of on_client function, sends some byte to server, closes on return and then it happens all over again. Should remove timers, because message flood to server is bad.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,120 @@
-/* 
- * File:   main.cpp
- * Author: Ivan
- * !!CLIENT!!
- * Here is a main file for network application. It uses znet lib: https://github.com/starwing/znet
- */
-/* tell znet include all implement details into here */ 
 #define ZN_IMPLEMENTATION
-#include <cstdlib>
-#include <stdio.h>
 #include "znet.h"
-#include <stdint.h>
+#include "zn_buffer.h"
+#include "zn_bufferpool.h"
 
-#define MYDATA_BUFLEN 1024
 
-char     addr[ZN_MAX_ADDRLEN] = "127.0.0.1";
+#include <stdio.h>
+#include <string.h>
+
+
+zn_State *S;
+zn_BufferPool pool;
+
+#define INTERVAL 5000
+#define DATA_SIZE 1024
+
+char addr[ZN_MAX_ADDRLEN];
+char data[DATA_SIZE+2];
 unsigned port = 8081;
+int send_count = 0;
+int recv_count = 0;
+int connect_count = 0;
 
-typedef struct MyData {
-    char buffer[MYDATA_BUFLEN];
-    int idx;
-    int count;
-} MyData;
+static void on_send(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
+    zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
+    if (err != ZN_OK) {
+        zn_putbuffer(&pool, node);
+        zn_deltcp(tcp);
+        return;
+    }
+    send_count += count;
+    zn_sendfinish(&node->send, count);
+    if (node->user_data == 4) {
+        zn_deltcp(tcp);
+        zn_putbuffer(&pool, node);
+    }
+}
 
-using namespace std;
-zn_State  *S;         /* the znet event loop handler */
-zn_Tcp    *tcp;       /* znet tcp client handler     */
-MyData    *data;      /* our user data pointer       */
+static void on_recv(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
+    zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
+    if (err != ZN_OK) {
+        zn_putbuffer(&pool, node);
+        zn_deltcp(tcp);
+        return;
+    }
+    recv_count += count;
+    zn_recvfinish(&node->recv, count);
+    zn_recv(tcp,
+        zn_recvbuff(&node->recv),
+        zn_recvsize(&node->recv), on_recv, ud);
+}
 
-/* we make a macro to compute a literal string's length, and put
- * length after string. */
-#define send_string(str) str, sizeof(str)-1
+static size_t on_header(void *ud, const char *buff, size_t len) {
+    unsigned short plen;
+    if (len < 2) return 0;
+    memcpy(&plen, buff, 2);
+    return ntohs(plen);
+}
 
-//----------CALLBACK PROTOTYPES----------
-/* function when a client is connected on a server. */
-void on_connection(void *ud, zn_Tcp *tcp, unsigned err);
-/* function when a tcp in client received something. */
-void on_client_recv(void *ud, zn_Tcp *tcp, unsigned err, unsigned count);
-/* function when a tcp in client sent something. */
-void on_client_sent(void *ud, zn_Tcp *tcp, unsigned err, unsigned count);
-//---------------------------------------
+static void on_packet(void *ud, const char *buff, size_t len) {
+    zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
+    if (zn_sendprepare(&node->send, buff, len))
+        zn_send(node->tcp,
+            zn_sendbuff(&node->send),
+            zn_sendsize(&node->send), on_send, node);
+    ++node->user_data;
+}
 
-//this code is required to handle CTRL-C interrupts in console app in a propper way
-//deinit and free all the resources
+static void on_connect(void *ud, zn_Tcp *tcp, unsigned err) {
+    zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
+    if (err != ZN_OK) {
+        zn_putbuffer(&pool, node);
+        zn_deltcp(tcp);
+        return;
+    }
+    ++connect_count;
+    zn_recv(tcp,
+        zn_recvbuff(&node->recv),
+        zn_recvsize(&node->recv), on_recv, ud);
+    if (zn_sendprepare(&node->send, data, DATA_SIZE+2))
+        zn_send(tcp,
+            zn_sendbuff(&node->send),
+            zn_sendsize(&node->send), on_send, ud);
+}
+/*
+static zn_Time on_client(void *ud, zn_Timer *timer, zn_Time elapsed) {
+    zn_BufferPoolNode *node = zn_getbuffer(&pool);
+    zn_Tcp *tcp = zn_newtcp(S);
+    zn_recvonheader(&node->recv, on_header, node);
+    zn_recvonpacket(&node->recv, on_packet, node);
+    node->user_data = 0;
+    node->tcp = tcp;
+    zn_connect(tcp, addr, port, on_connect, node);
+    return 1;
+}
+*/
+static void init_data(void) {
+    int count = 2;
+    const char *padding = "Hello world\r\n";
+    size_t paddlen = strlen(padding);
+    short len = htons(DATA_SIZE);
+    memcpy(data, &len, 2);
+    while (count < DATA_SIZE) {
+        strcpy(data+count, padding);
+        count += paddlen;
+    }
+}
+
+static zn_Time on_timer(void *ud, zn_Timer *timer, zn_Time elapsed) {
+    printf("%d: connect=%d, recv=%d, send=%d\n",
+            zn_time(), connect_count, recv_count, send_count);
+    connect_count = 0;
+    recv_count = 0;
+    send_count = 0;
+    return INTERVAL;
+}
+
 static void cleanup(void) {
     printf("exiting ... ");
     zn_close(S);
@@ -50,6 +123,7 @@ static void cleanup(void) {
     zn_deinitialize();
     printf("OK\n");
 }
+
 #ifdef _WIN32
 static int deinited = 0;
 static BOOL WINAPI on_interrupted(DWORD dwCtrlEvent) {
@@ -79,122 +153,33 @@ static void register_interrupted(void) {
    sigaction(SIGINT, &act, NULL);
 }
 #endif
-//CALLBACKS
-/* the client connection callback: when you want to connect other
- * server, and it's done, this function will be called. */
-void on_connection(void *ud, zn_Tcp *tcp, unsigned err) {
-    MyData *data = (MyData*)ud;
-    if (err != ZN_OK) { /* no lucky? let's try again. */
-        /* we use ud to find out which time we tried. */
-        fprintf(stderr, "[%p] client%d can not connect to server now: %s\n",
-                tcp, data->idx, zn_strerror(err));
-        if (++data->count < 10) {
-            fprintf(stderr, "[%p client%d just try again (%d times)! :-/ \n",
-                    tcp, data->idx, data->count);
-            zn_connect(tcp, "127.0.0.1", 8080, on_connection, data);
-        }
-        else {
-            fprintf(stderr, "[%p] client%d just give up to connect :-( \n",
-                    tcp, data->idx);
-            zn_deltcp(tcp);
-            free(data);
-        }
-        return;
+
+int main(int argc, char **argv) {
+    zn_Timer *t1, *t2;
+    strcpy(addr, "127.0.0.1");
+    if (argc == 2) {
+        unsigned p = atoi(argv[1]);
+        if (p != 0) port = p;
+    }
+    else if (argc == 3) {
+        unsigned p = atoi(argv[2]);
+        strcpy(addr, argv[1]);
+        if (p != 0) port = p;
     }
 
-    printf("[%p] client%d connected to server now!\n", tcp, data->idx);
-
-    /* now we connect to the server, send something to server.
-     * when send done, on_send() is called.  */
-    /*zn_send(tcp, send_string("Hello world\n"), on_send, NULL);*/
-
-    /* but, we want not just send one message, but five messages to
-     * server. how we know which message we sent done? a idea is
-     * setting many callback functions, but the better way is use a
-     * context object to hold memories about how many message we sent.
-     * */
-    data->count = 0;
-    zn_send(tcp, send_string("this is the first message from client!"),
-            on_client_sent, data);
-
-    /* and we raise a request to make znet check whether server send us
-     * something ... */
-    zn_recv(tcp, data->buffer, MYDATA_BUFLEN, on_client_recv, data);
-}
-
-void on_client_sent(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
-    MyData *data = (MyData*)ud;
-
-    /* send work may error out, we first check the result code: */
-    if (err != ZN_OK) {
-        fprintf(stderr, "[%p] client%d meet problem when send something: %s\n",
-                tcp, data->idx, zn_strerror(err));
-        zn_deltcp(tcp); /* and we close connection. */
-        free(data);
-        return;
-    }
-
-    if (++data->count > 5) {
-        printf("[%p] client%d ok, 5 messages sent and not even closing\n", tcp, data->idx);
-        ++data->count = 0;
-    }
-
-    printf("[%p] client%d send messages success!\n", tcp, data->idx);
-    printf("[%p] client%d send message%d to server ...\n",
-            tcp, data->idx, data->count);
-    //zn_send(tcp, send_string("message from client...\n"), on_client_sent, data);
-}
-
-void on_client_recv(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
-    MyData *data = (MyData*)ud; /* our data from zn_recv() */
-
-    if (err != ZN_OK) {
-        fprintf(stderr, "[%p] client%d meet error when receiving: %s",
-                tcp, data->idx, zn_strerror(err));
-        return;
-    }
-
-    fprintf(stderr, "[%p] client%d received from server: %.*s (%d bytes)\n",
-            tcp, data->idx, (int)count, data->buffer, (int)count);
-}
-//---------
-int main(int argc, char** argv) {
-    printf("znet example: use %s engine.\n", zn_engine());
-        /* first, we initialize znet global environment. we needn't do
-     * this on *nix or Mac, because on Windows we should initialize
-     * and free the WinSocks2 global service, the
-     * zn_initialize()/zn_deinitialize() function is prepared for this
-     * situation. */
+    init_data();
     zn_initialize();
-        /* after network environment is ready, we can create a new event
-     * loop handler now. the zn_State object is the center object that
-     * hold any resource znet uses, when everything overs, you should
-     * call zn_close() to clean up all resources znet used. */
     S = zn_newstate();
-    if (S == NULL) {
-        fprintf(stderr, "create znet handler failured\n");
-        return 2; /* error out */
-    }
-    /* Declare callback to release resources on exit (on CTRL+C) */
-    register_interrupted();
-    /* now connect to the server */
-    data = (MyData*)malloc(sizeof(MyData));
-    data->idx = 1;
-    data->count = 0;
-    tcp = zn_newtcp(S);
-    zn_connect(tcp, addr, port, on_connection, data);
-    printf("My Network Client.");
-    uint8_t stopped = 0;
-    zn_run(S, ZN_RUN_LOOP);
-    while (stopped == 0) {
-        //  https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf
-        fgets(data->buffer, MYDATA_BUFLEN, stdin);
-        if (strlen(data->buffer)>0 && (data->buffer[strlen (data->buffer) - 1] == '\n') ) {
-            data->buffer[strlen (data->buffer) - 1] = '\0';
-        }
-        if (strlen(data->buffer) == 0) {continue;}
-        zn_send(tcp, send_string(data->buffer), on_client_sent, data);
-    }
-    return 0;
-}
+    zn_initbuffpool(&pool);
+    if (S == NULL)
+        return 2;
+    printf("!!Fancy znet network client!!\n");
+//    t1 = zn_newtimer(S, on_timer, NULL);
+//    zn_starttimer(t1, INTERVAL);
+//    t2 = zn_newtimer(S, on_client, NULL);
+//    zn_starttimer(t2, 0);
 
+    register_interrupted();
+    return zn_run(S, ZN_RUN_LOOP);
+}
+/* cc: flags+='-s -O3' libs+='-lws2_32' */
