46db4f9a5538d54ac87b897b1797175ae5340278
uzername <ivanpost777@gmail.com>
2018-01-04 20:03:48 +0200

    making message

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 #include <stdio.h>
 #include <string.h>
 
-
+//cycle handler. main service of znet library
 zn_State *S;
+//client instance (structure)
+zn_Tcp *tcpGlobal;
+/*the interface is targeted by send/recv method, so it's very useful to make znet 
+ * send data continually and receive packets by packets.*/
 zn_BufferPool pool;
 
 #define INTERVAL 5000
@@ -20,7 +24,7 @@ unsigned port = 8081;
 int send_count = 0;
 int recv_count = 0;
 int connect_count = 0;
-
+//callback after sending
 static void on_send(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
     zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
     if (err != ZN_OK) {
@@ -30,12 +34,14 @@ static void on_send(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
     }
     send_count += count;
     zn_sendfinish(&node->send, count);
+    /*
     if (node->user_data == 4) {
         zn_deltcp(tcp);
         zn_putbuffer(&pool, node);
     }
+    */
 }
-
+//reception callback
 static void on_recv(void *ud, zn_Tcp *tcp, unsigned err, unsigned count) {
     zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
     if (err != ZN_OK) {
@@ -65,7 +71,7 @@ static void on_packet(void *ud, const char *buff, size_t len) {
             zn_sendsize(&node->send), on_send, node);
     ++node->user_data;
 }
-
+//connection callback, it is called after client is connected to server
 static void on_connect(void *ud, zn_Tcp *tcp, unsigned err) {
     zn_BufferPoolNode *node = (zn_BufferPoolNode*)ud;
     if (err != ZN_OK) {
@@ -94,6 +100,17 @@ static zn_Time on_client(void *ud, zn_Timer *timer, zn_Time elapsed) {
     return 1;
 }
 */
+//perform connection
+static void init_client() {
+    zn_BufferPoolNode *node = zn_getbuffer(&pool);
+    tcpGlobal = zn_newtcp(S);
+    zn_recvonheader(&node->recv, on_header, node);
+    zn_recvonpacket(&node->recv, on_packet, node);
+    node->user_data = 0;
+    node->tcp = tcpGlobal;
+    zn_connect(tcpGlobal, addr, port, on_connect, node);
+}
+
 static void init_data(void) {
     int count = 2;
     const char *padding = "Hello world\r\n";
@@ -173,13 +190,19 @@ int main(int argc, char **argv) {
     zn_initbuffpool(&pool);
     if (S == NULL)
         return 2;
-    printf("!!Fancy znet network client!!\n");
+    printf("!!Fancy znet network client (CTRL+C to exit)!!\n");
 //    t1 = zn_newtimer(S, on_timer, NULL);
 //    zn_starttimer(t1, INTERVAL);
 //    t2 = zn_newtimer(S, on_client, NULL);
 //    zn_starttimer(t2, 0);
-
+    init_client();
+    printf("%s :: %d\n", addr, port);
     register_interrupted();
-    return zn_run(S, ZN_RUN_LOOP);
+    zn_run(S, ZN_RUN_LOOP);
+    while (true) {
+          
+    }
+    
+    return 0;
 }
 /* cc: flags+='-s -O3' libs+='-lws2_32' */
